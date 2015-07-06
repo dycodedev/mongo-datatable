3ce632f8e2e28c64b2705dcc50754357d0127c32
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-07-06 11:01:12 +0700

    No longer using legacy version of initialization script

--- public/javascripts/initialize-datatables.js
+++ public/javascripts/initialize-datatables.js
@@ -1,21 +1,21 @@
 $(function() {
   $('#subscriptions-table').DataTable({
-    sPaginationType: 'full_numbers',
-    bServerSide: true,
-    bProcessing: true,
-    bAutoWidth: false,
-    sAjaxSource: "http://127.0.0.1:3000/subscriptions",
+    serverSide: true,
+    pocessing: true,
+    autoWidth: false,
+    ajax: 'subscriptions',
     aoColumns: [
-      { 'mData': '_id', 'sTitle': 'Id' },
-      { 'mData': 'subs_name', 'sTitle': 'Name' },
-      { 'mData': 'subs_screen_name', 'sTitle': 'Screen Name' },
+      { 'data': '_id', 'name': '_id' },
+      { 'data': 'subs_name', 'name': 'subs_name' },
+      { 'data': 'subs_screen_name', 'name': 'subs_screen_name' },
       { 
-        'mData': 'services', 
-        'mRender': function (data) {
+        'data': 'services', 
+        'name': 'services',
+        'render': function (data) {
           return data.join(',');
         }
       },
-      { 'mData': 'state'}
+      { 'data': 'state', 'name': 'state'}
     ]
   });
 });
