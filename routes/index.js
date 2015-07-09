c1ea4565eba93ddbf638500818dd8fa8afacbbf3
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-07-09 12:02:50 +0700

    Using new module

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
 var express = require('express');
+var MongoDataTable = require('mongo-datatable');
+var mongodb = require('mongodb');
+var Db = mongodb.Db;
+var Server = mongodb.Server;
 var router = express.Router();
-var initDataTables = require('../lib/init-datatables');
 var config = require('../config/dev.dycode');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'DataTables with NodeJS & MongoDB' });
 });
 
-router.get('/subscriptions', 
-  initDataTables(config.mongodb.connectionUri,'subscriptions')
-);
+router.get('/subscriptions', function(req, res, next) {
+  var server = new Server(config.mongodb.host, config.mongodb.port);
+  var db = new Db(config.mongodb.dbname, server);
+
+  db.open(function(error, db) {
+    new MongoDataTable(db).get('subscriptions', req.query, function(error, result) {
+      if (error) {
+        console.error(error);
+      }
+
+      res.json(result);
+    });
+  });
+});
 
 module.exports = router;
--- routes/zipcodes.js
+++ routes/zipcodes.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var initDataTables = require('../lib/init-datatables');
 var config = require('../config/dev.local');
-
+var MongoDataTable = require('mongo-datatable');
+var MongoClient = require('mongodb').MongoClient;
 
 router.get('/', function(req, res, next) {
   res.render('zipcodes', { title: 'MongoDB & DataTables' });
 });
 
-router.get('/zipcodes.json', 
-  initDataTables(config.mongodb.connectionUri, 'zipcodes')
-);
+router.get('/zipcodes.json', function(req, res, next) {
+  MongoClient.connect(config.mongodb.connectionUri, function(error, db) {
+    var mongodt = new MongoDataTable(db);
+    mongodt.get('zipcodes', req.query, returnData);
+
+    function returnData(error, result) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      res.json(result);
+    }
+  });
+});
 
 module.exports = router;
