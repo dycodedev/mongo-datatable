2c4e7bd4573436d87e8d05839d1f01b9c6048f76
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-07-09 12:03:13 +0700

    Support multi column search

--- public/javascripts/initialize-datatables.js
+++ public/javascripts/initialize-datatables.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-  $('#subscriptions-table').DataTable({
+  $('#subscriptions-table tfoot th').each(function() {
+    var title = $('#subscriptions-table thead th').eq($(this).index()).text();
+    $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+  });
+
+  var table = $('#subscriptions-table').DataTable({
     serverSide: true,
     autoWidth: false,
     processing: true,
     ajax: 'subscriptions',
     columns: [
-      { 'data': '_id', 'name': '_id' },
+      { 'data': '_id', 'name': '_id', 'searchable': false },
       { 'data': 'subs_name', 'name': 'subs_name' },
       { 'data': 'subs_screen_name', 'name': 'subs_screen_name' },
       { 
@@ -22,4 +27,15 @@ $(document).ready(function() {
   $('#subscriptions-table')
     .removeClass('display')
     .addClass('table table-bordered table-stripped');
+
+  table.columns().every(function() {
+    var that = this;
+    console.log(that);
+
+    $('input', this.footer()).on('keyup change', function() {
+      that
+        .search(this.value)
+        .draw();
+    });
+  });
 });
