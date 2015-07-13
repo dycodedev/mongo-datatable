efa3cae1bc5b050321253d9a4e4655be5f926a18
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-07-13 12:46:21 +0700

    Added test with multiple column search

--- test/public/javascripts/init-zipcodes-datatable.js
+++ test/public/javascripts/init-zipcodes-datatable.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
-  $('#zipcodes-table').DataTable({
+  $('#zipcodes-table tfoot th').each(function() {
+    var title = $('#zipcodes-table tfoot th').eq($(this).index()).text();
+    $(this).html('<input type="text" placeholder="Search ' + title + '"/>');
+  });
+
+  var table = $('#zipcodes-table').DataTable({
     serverSide: true,
     processing: true,
     autoWidth: false,
@@ -21,4 +26,14 @@ $(document).ready(function() {
   $('#zipcodes-table')
     .removeClass('display')
     .addClass('table table-stripped table-bordered');
+
+  table.columns().every(function() {
+    var that = this;
+
+    $('input', this.footer()).on('keyup change', function() {
+      that
+        .search(this.value)
+        .draw();
+    });
+  });
 });
