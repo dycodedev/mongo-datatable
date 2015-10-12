1aa696da2ecf50063b0fe028986b0efa6b70c9a3
Alwin Arrasyid <alwin.ridd@gmail.com>
2015-10-12 16:38:50 +0700

    Using lodash to process custom query

--- test/routes/index.js
+++ test/routes/index.js
@@ -11,9 +11,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MongoDB Datatable' });
 });
 
-router.get('/zipcodes.json', function(req, res) { 
+router.get('/zipcodes.json', function(req, res) {
   var options = req.query;
   options.showAlert = true;
+
+  // Select data with state MA and city name that starts with B and population less than 2000
+  options.customQuery = {
+    state: 'MA',
+    city: /^B/,
+    pop: { $lt: 2000 }
+  };
 
   MongoClient.connect(config.mongodb.connectionUri, function(err, db) {
     if (err) {
